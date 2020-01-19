4d9c0563f5c1b988552f42e455b05ea31b180202
chaewon <chaewonyun21@gmail.com>
2020-01-19 23:04:12 +0900

    add syllabus list

--- scrape.py
+++ scrape.py
@@ -2,6 +2,7 @@
 #-*-coding: UTF-8 -*-
 from lxml import html
 import codecs
+import json
 from googletrans import Translator
 import requests
 import pdb
@@ -15,16 +16,15 @@ tr_pairs=dict()
 #translator = Translator(proxies=proxy)# .sleep(.4)
 translator = Translator()
 page = requests.get(url)
-#pdb.set_trace()
 tree = html.fromstring(page.content)
 
 def my_trans(string):
-    string = string.replace(u'\xa0', u' ').replace(u'\t', u'').replace(u'\n', u'').replace(u'\0', u'').strip() # .encode('utf8')
+    string = string.replace('\xa0', ' ').replace('\0', '').strip() #sub.replace('\t', '').replace('\n', '').replace('\0', '').strip() # .encode('utf8')
     if string:
         print('string:{}'.format(string))
         return translator.translate(string, dest='en', src='ko').text
     return string
-
+'''
 box = tree.xpath('//td[@class="BoxText_1"]/text()')
 pre = tree.xpath('//pre/text()')
 for btext in box:
@@ -36,12 +36,19 @@ for ptext in pre:
 
 dict_fh = codecs.open('dict_fh', 'w', encoding='utf8')
 dict_fh.write(str(tr_pairs))
+'''
 # fh = codecs.open(url, 'r', encoding='utf8')
 # html_text = '\n'.join(fh.readlines())
-page_txt = page.content.encode('utf8')
-for key in tr_pairs.keys():
+
+page_fh = codecs.open('pagepage', 'w', encoding='utf8')
+string_dict = codecs.open('dict_fh', 'r', encoding='utf8')# .read() #str
+maybe_dict = json.load(string_dict) #, 'r', encoding='utf8')
+page_txt = page.content.decode('cp949', 'ignore')
+# for key in tr_pairs.keys():
+for key in maybe_dict.keys():
     # html_text = html_text.sub(key, tr_pairs[key])
-    page_txt = page_txt.sub(key, tr_pairs[key])
+    page_txt = page_txt.replace(key.strip(), maybe_dict[key])
+    print(key.strip() in page_txt)
+    #print(maybe_dict[key])
 page_fh.write(page_txt)
-dict_fh.close()
-pdb.set_trace()
+# dict_fh.close()
